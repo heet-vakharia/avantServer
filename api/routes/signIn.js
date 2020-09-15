70e23945b2e5cd393362c8b14c923bbe06694ab4
Heet Vakharia <heet1476@gmail.com>
2020-09-15 16:54:27 +0530

    Fixed issues

--- api/routes/signIn.js
+++ api/routes/signIn.js
@@ -1,19 +1,23 @@
 const signInRoute = async (req, res, User, bcrypt, sendEmail) => {
   const { userUserid, userPassword } = req.body;
-  await User.findOne({ userid: userUserid }, (err, user) => {
-    if (err || !user) {
-      res.status(401).json("Wrong Username");
-    } else {
-      const { userid, password, _id, email } = user;
-      bcrypt.compare(userPassword, password, (err, resp) => {
-        if (resp) {
-          // sendEmail(email, `A new signin from your account`);
-          res.status(302).json({ userid, email, id: _id });
-        } else {
-          res.status(404).json({ err: "Invalid Password" });
-        }
-      });
-    }
-  });
+  if (userUserid && userPassword) {
+    await User.findOne({ userid: userUserid }, (err, user) => {
+      if (err || !user) {
+        res.status(401).json("Wrong Username");
+      } else {
+        const { userid, password, _id, email } = user;
+        bcrypt.compare(userPassword, password, (err, resp) => {
+          if (resp) {
+            // sendEmail(email, `A new signin from your account`);
+            res.status(302).json({ userid, email, id: _id });
+          } else {
+            res.status(404).json({ err: "Invalid Password" });
+          }
+        });
+      }
+    });
+  } else {
+    res.status(400).json({ msg: "Plz provide all information" });
+  }
 };
 export default signInRoute;
